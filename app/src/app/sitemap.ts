a0f36ae22ebe2b1b69e485713e16b39084ce46d3
hjacquir <jacquirhatim@gmail.com>
2026-06-11 15:24:18 +0200

    ajout de la base url dans le sitemap

--- app/src/app/sitemap.ts
+++ app/src/app/sitemap.ts
@@ -2,8 +2,10 @@ import {MetadataRoute} from 'next'
 import data from '@/src/data/urls.json'
 
 export default function sitemap(): MetadataRoute.Sitemap {
+    const baseUrl = process.env.PUBLIC_BASE_URL
+
     return data.map((item) => ({
-        url: item.url,
+        url: `${baseUrl}${item.url}`,
         lastModified: item.lastModified,
         changeFrequency: item.changeFrequency as MetadataRoute.Sitemap[number]['changeFrequency'],
         priority: item.priority
